e090749673144a642c102c1ba1c96bebf63373c0
Germana Oliveira <germana@vauxoo.com>
2017-05-18 17:30:17 +0000

    [FIX] openacademy: Fix session test error

--- openacademy/tests/test_global_openacademy_session.py
+++ openacademy/tests/test_global_openacademy_session.py
@@ -22,7 +22,7 @@ class GlobalTestOpenAcademySession(TransactionCase):
         '''
         with self.assertRaisesRegexp(
             ValidationError,
-            "A session's instructor can't be an attenidee"
+            "A session's instructor can't be an attendee"
         ):
             self.session.create({
                 'name': 'Session test 1',
